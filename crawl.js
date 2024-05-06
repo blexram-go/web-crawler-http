f7b856bc94c5cbd5d9e8e77bf48d92e7bb2be823
brianalexramos <brian_ramos_14@yahoo.com>
2024-05-06 12:54:59 -0700

    Add report functionality as well ability to crawl through more links

--- crawl.js
+++ crawl.js
@@ -2,7 +2,7 @@ import { JSDOM } from 'jsdom';
 
 function normalizeURL(baseURL) {
     const normalizedURL = new URL(baseURL);
-    let fullPath = `${normalizedURL.hostname}${normalizedURL.pathname}`;
+    let fullPath = `${normalizedURL.host}${normalizedURL.pathname}`;
 
     if (fullPath.slice(-1) === '/') {
         fullPath = fullPath.slice(0, -1);
@@ -31,4 +31,53 @@ function getURLsFromHTML(body, url) {
 
 }
 
-export { normalizeURL, getURLsFromHTML };
+async function fetchHTML(currentURL) {
+    let response;
+    try {
+        response = await fetch(currentURL);
+    } catch (err) {
+        throw new Error(`Got Network Error: ${err.message}`);
+    }
+
+    if (response.status > 399) {
+        throw new Error(`Got HTTP Error: ${response.status} ${response.statusText}`);
+    }
+    
+    const contentType = response.headers.get('content-type');
+    if (!contentType || !contentType.includes("text/html")) {
+        throw new Error(`Got non-HTML response: ${contentType}`);
+    }
+    return response.text();
+}
+
+async function crawlPage(baseURL, currentURL=baseURL, pages={}) {
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages;
+    }
+
+    const normalizedURL = normalizeURL(currentURL);
+    if (pages[normalizedURL] > 0) {
+        pages[normalizedURL]++;
+        return pages;
+    }
+    pages[normalizedURL] = 1;
+
+    console.log(`crawling ${currentURL}`);
+    let html = '';
+    try {
+        html = await fetchHTML(currentURL);
+    } catch (err) {
+        console.log(`${err.message}`);
+        return pages;
+    }
+
+    const nextURLs = getURLsFromHTML(html, baseURL);
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages);
+    }
+    return pages;
+}
+
+export { normalizeURL, getURLsFromHTML, crawlPage };
--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('hello world!');
+import { crawlPage } from "./crawl.js";
+import { printReport } from "./report.js";
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log("Error! Missing the website argument.");
+    } else if (process.argv.length > 3) {
+        console.log("Error! Too many arguments.");
+    }
+
+    const baseURL = process.argv[2];
+    console.log(`starting crawl of ${baseURL}`);
+    const pages = await crawlPage(baseURL);
+    printReport(pages);
+};
+
+main();
